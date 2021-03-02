8cfea81586d4cb0768bf7d98ad9295f4eb128baa
Ran Levi <ran.levi@gmail.com>
2021-03-02 08:57:08 +0200

    Added simple animation

--- js.js
+++ js.js
@@ -1,3 +1,7 @@
+let random_num = Math.random();
+const SPEED = 0.3;
+const OFFSET = 0.01;
+
 const canvas = document.querySelector("#renderCanvas")
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 let   scene =  new BABYLON.Scene(engine);
@@ -7,11 +11,31 @@ camera.setTarget(BABYLON.Vector3.Zero());
 camera.maxZ = 100;
 camera.minZ = 1 ;
 
-let light = new BABYLON.DirectionalLight("DirectionalLight", new BABYLON.Vector3(1, 0, 0), scene);
-light.setPosition(0,0,-10);
+let light = new BABYLON.PointLight("DirectionalLight", new BABYLON.Vector3(0, 0, -11), scene);
+
 const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
+box.position.z = 90;
 
 engine.runRenderLoop(function () {
+    
+    box.position.z -= 1*SPEED;
+    if (box.position.z<=-10) {
+        box.position = new BABYLON.Vector3(0,0,90);
+        random_num = Math.random();
+        console.log(random_num);
+    }
+    
+    if (random_num<0.25){
+        box.position.x += OFFSET;
+    } else if (random_num>=0.25 && random_num<0.5){
+        box.position.x -= OFFSET;
+    } else if (random_num>=0.5 && random_num<0.75){
+        box.position.y += OFFSET;
+    } else {
+        box.position.y -= OFFSET;
+    }
+        
+
     scene.render();
 });
 
