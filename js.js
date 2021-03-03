7ded3274e8d37a8bc8657c4cd38d40cb9a1d6314
Ran Levi <ran.levi@gmail.com>
2021-03-03 16:10:00 +0200

    added lines

--- js.js
+++ js.js
@@ -13,22 +13,48 @@ camera.minZ = 1 ;
 
 let light = new BABYLON.PointLight("DirectionalLight", new BABYLON.Vector3(0, 0, -11), scene);
 
+const Mat = new BABYLON.StandardMaterial("Mat", scene);
+Mat.diffuseColor = new BABYLON.Color3(0,1,0);
+
 const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
 box.position.z = 90;
+box.material = Mat;
 
 let random = generate_randomness();
 box.position = new BABYLON.Vector3(random.X_OFFSET,random.Y_OFFSET,90);
 box.scaling  = new BABYLON.Vector3(random.X_SCALE,random.Y_SCALE,random.Z_SCALE);
 
+let points = [];
+points.push(new BABYLON.Vector3(2, -2, 0));
+points.push(new BABYLON.Vector3(2, 2, 0));
+points.push(new BABYLON.Vector3(-2, 2, 0));
+points.push(new BABYLON.Vector3(-2, -2, 0));
+points.push(points[0]); //close the triangle;
+let cube = BABYLON.MeshBuilder.CreateLines("triangle", {points: points})
+
+points = [
+    points.push(new BABYLON.Vector3(2, -2, 0));
+];
+
+
 engine.runRenderLoop(function () {
     
     box.position.z -= 1.1;
+    cube.rotation.z += BABYLON.Tools.ToRadians(5);
+
+    if (random.UNITY<0.5){
+        box.rotation.x += BABYLON.Tools.ToRadians(5);    
+    } else {
+        box.rotation.y += BABYLON.Tools.ToRadians(5);
+    }
     
     if (box.position.z<=-10) {
         random = generate_randomness();
 
         box.position = new BABYLON.Vector3(random.X_OFFSET,random.Y_OFFSET,90);
         box.scaling  = new BABYLON.Vector3(random.X_SCALE,random.Y_SCALE,random.Z_SCALE);
+
+        
         // console.log(box.position);
         
     }
@@ -45,6 +71,7 @@ window.addEventListener("resize", function () {
 
 function generate_randomness(){
     let obj = {
+        UNITY:    Math.random(),//[0,1]
         X_OFFSET: Math.floor(20*Math.random()-10),//[-10,10]
         Y_OFFSET: Math.floor(20*Math.random()-10),//[-10,10]
         X_SCALE:  3*Math.random(),//[0,3]
