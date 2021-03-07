f7669fa6189961736cf7fcdd1975c57ee09b4e9e
Ran Levi <ran.levi@gmail.com>
2021-03-07 09:56:25 +0200

    added cam

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 //Space change scene
 //camera moves along a 3d line. make lots of points. move position on them. 
-//Control - mouse. WASD, like plane.
+//can have guided tour mode , or manual moving
+
 //Music - spotify/sound clound? 
 
 //Reddit integration:
@@ -12,27 +13,24 @@ Image gallery
 const canvas= document.querySelector("#renderCanvas")
 const engine= new BABYLON.Engine(canvas, true);
 let   scene=  new BABYLON.Scene(engine);
-// scene.clearColor = BABYLON.Color3.FromHexString("#2ecc71");
 
 //Camera Path
 let bezier2 = BABYLON.Curve3.CreateQuadraticBezier(
   new BABYLON.Vector3(10,0,-10), //Start
-  new BABYLON.Vector3(7,4,-7), //Control
-  new BABYLON.Vector3(10,10,0), //Destination
-  100 //Num of points
+  new BABYLON.Vector3(7,4,-7),   //Control
+  new BABYLON.Vector3(10,10,0),  //Destination
+  100                            //Num of points
 ); 
 
 let cam_points = bezier2.getPoints();
+let current_position = 0;
 
-let camera=  new BABYLON.UniversalCamera("UniversalCamera", cam_points[0], scene);
+let camera=  new BABYLON.UniversalCamera("UniversalCamera", cam_points[current_position], scene);
 camera.setTarget(BABYLON.Vector3.Zero());
 camera.maxZ= 100;
 camera.minZ= 1 ;
-
-// var camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 0, -10), scene);
-// camera.setTarget(BABYLON.Vector3.Zero());
-// var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene);
-// camera.attachControl(canvas, true);
+camera.attachControl(canvas, true);
+camera.inputs.removeByType("FreeCameraKeyboardMoveInput");
 
 let light = new BABYLON.PointLight("DirectionalLight", new BABYLON.Vector3(0, 0, -11), scene);
 
@@ -43,47 +41,6 @@ const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
 // box.position.z = 90;
 box.material = Mat;
 
-// let random = generate_randomness();
-// box.position = new BABYLON.Vector3(random.X_OFFSET,random.Y_OFFSET,90);
-// box.scaling  = new BABYLON.Vector3(random.X_SCALE,random.Y_SCALE,random.Z_SCALE);
-
-// let points = [];
-// points.push(new BABYLON.Vector3(2, -2, 0));
-// points.push(new BABYLON.Vector3(2, 2, 0));
-// points.push(new BABYLON.Vector3(-2, 2, 0));
-// points.push(new BABYLON.Vector3(-2, -2, 0));
-// points.push(points[0]); //close the triangle;
-// let cube = BABYLON.MeshBuilder.CreateLines("triangle", {points: points})
-
-// points = [];
-// points.push(new BABYLON.Vector3(2, 0, 0));
-// points.push(new BABYLON.Vector3(2, -1, 0));
-// points.push(new BABYLON.Vector3(1, -2, 0));
-// let lat = BABYLON.MeshBuilder.CreateLathe("lat", {shape: points}, scene);
-
-// const adt = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-// const panel = new BABYLON.GUI.StackPanel();
-// panel.width = "220px";
-// panel.top   = "-50px";
-// panel.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
-// panel.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
-// adt.addControl(panel);
-// const slider = new BABYLON.GUI.Slider();
-// slider.minimum = 0;
-// slider.maximum = 1;
-// slider.borderColor = "black";
-// slider.color = "#AAAAAA";
-// slider.background = "#white";
-// slider.value = 1;
-// slider.height = "20px";
-// slider.width = "200px";
-// panel.addControl(slider);
-
-// let speed = 0.5;
-
-// slider.onValueChangedObservable.add((value) => {
-//   speed = 0.5 + value;
-// });
 
 engine.runRenderLoop(function () {
     
@@ -132,16 +89,66 @@ function generate_randomness(){
 
 scene.onKeyboardObservable.add((kbInfo) => {
 
-  console.log(kbInfo.event.keyCode);
+//   console.log(kbInfo.event.keyCode);
 
   if (kbInfo.type===BABYLON.KeyboardEventTypes.KEYDOWN){
     if (kbInfo.event.keyCode==32){
       scene.clearColor = BABYLON.Color3.FromHexString("#2eaaaa");
     } else if (kbInfo.event.keyCode==90){ //z
-      // camera.setPosition(new BABYLON.Vector3(0, 0, 20));
-      camera.position = new BABYLON.Vector3(0, 0, 0);
+    
+      current_position += 1;
+      camera.position = cam_points[current_position];
+    //   camera.setTarget(BABYLON.Vector3.Zero());
+
+    //   console.log(camera.position);
+
     }
   } 
 
 });
 
+// scene.clearColor = BABYLON.Color3.FromHexString("#2ecc71");
+// let random = generate_randomness();
+// box.position = new BABYLON.Vector3(random.X_OFFSET,random.Y_OFFSET,90);
+// box.scaling  = new BABYLON.Vector3(random.X_SCALE,random.Y_SCALE,random.Z_SCALE);
+
+// let points = [];
+// points.push(new BABYLON.Vector3(2, -2, 0));
+// points.push(new BABYLON.Vector3(2, 2, 0));
+// points.push(new BABYLON.Vector3(-2, 2, 0));
+// points.push(new BABYLON.Vector3(-2, -2, 0));
+// points.push(points[0]); //close the triangle;
+// let cube = BABYLON.MeshBuilder.CreateLines("triangle", {points: points})
+
+// points = [];
+// points.push(new BABYLON.Vector3(2, 0, 0));
+// points.push(new BABYLON.Vector3(2, -1, 0));
+// points.push(new BABYLON.Vector3(1, -2, 0));
+// let lat = BABYLON.MeshBuilder.CreateLathe("lat", {shape: points}, scene);
+
+// const adt = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+// const panel = new BABYLON.GUI.StackPanel();
+// panel.width = "220px";
+// panel.top   = "-50px";
+// panel.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+// panel.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+// adt.addControl(panel);
+// const slider = new BABYLON.GUI.Slider();
+// slider.minimum = 0;
+// slider.maximum = 1;
+// slider.borderColor = "black";
+// slider.color = "#AAAAAA";
+// slider.background = "#white";
+// slider.value = 1;
+// slider.height = "20px";
+// slider.width = "200px";
+// panel.addControl(slider);
+
+// let speed = 0.5;
+
+// slider.onValueChangedObservable.add((value) => {
+//   speed = 0.5 + value;
+// });
+// var camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 0, -10), scene);
+// camera.setTarget(BABYLON.Vector3.Zero());
+// var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene);
